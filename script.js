89385cde0dec685909739d97fb36bc4f6c3f5c75
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 03:07:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function firstWord(s) {
-  // your code here
+  // Check if the input string is empty
+  if (s.length === 0) {
+    return ''; // Return an empty string
+  }
+  
+  // Trim the string to remove leading spaces
+  const trimmedString = s.trim();
+  
+  // Find the index of the first space
+  const spaceIndex = trimmedString.indexOf(' ');
+  
+  // If there is no space, return the entire trimmed string
+  if (spaceIndex === -1) {
+    return trimmedString;
+  }
+  
+  // Return the substring from the start to the first space
+  return trimmedString.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
